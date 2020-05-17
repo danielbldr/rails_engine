3b3a4309a0ee740aac3a16acc640cf512ecac4fb
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-05-16 19:53:44 -0600

    Add rubocop linter and remove lines with spaces in invoices and transactions models

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,6 +2,6 @@ class Transaction < ApplicationRecord
   validates :credit_card_number, presence: true
   validates :card_expiration_date, presence: true
   validates :result, presence: true
-  
+
   belongs_to :invoice
 end
