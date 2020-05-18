3d2cea37e673c0688b7b04ec63102deef0b045e4
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-05-18 15:10:52 -0600

    Update tests per new JSON format with serializer and test it can create a new item

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -11,11 +11,13 @@ describe "Items API" do
 
     items = JSON.parse(response.body)
 
-    expect(items.count).to eq(3)
-    expect(items.first).to have_key('name')
-    expect(items.first).to have_key('description')
-    expect(items.first).to have_key('unit_price')
-    expect(items.first).to have_key('merchant_id')
+    expect(items['data'].count).to eq(3)
+    expect(items['data'].first['attributes']).to have_key('name')
+    expect(items['data'].first['attributes']).to have_key('description')
+    expect(items['data'].first['attributes']).to have_key('unit_price')
+    expect(items['data'].first['attributes']).to have_key('merchant_id')
+    expect(items['data'].first['attributes']).to_not have_key('updated_at')
+    expect(items['data'].first['attributes']).to_not have_key('created_at')
   end
 
   it "returns data for a specified item" do
@@ -28,9 +30,28 @@ describe "Items API" do
 
     item = JSON.parse(response.body)
 
-    expect(item).to have_key('name')
-    expect(item).to have_key('description')
-    expect(item).to have_key('unit_price')
-    expect(item).to have_key('merchant_id')
+    expect(item.count).to eq(1)
+    expect(item['data']['attributes']).to have_key('name')
+    expect(item['data']['attributes']).to have_key('description')
+    expect(item['data']['attributes']).to have_key('unit_price')
+    expect(item['data']['attributes']).to have_key('merchant_id')
+    expect(item['data']['attributes']).to_not have_key('updated_at')
+    expect(item['data']['attributes']).to_not have_key('created_at')
+  end
+
+  it "can create a new item" do
+    merchant = create(:merchant)
+    item_params = { name: 'Dark Roast', description: 'Burnt coffee',
+                     unit_price: 4.50, merchant_id: merchant.id}
+
+    post '/api/v1/items/', params: { item: item_params }
+
+    item = Item.last
+
+    expect(response).to be_successful
+    expect(item.name).to eq(item_params[:name])
+    expect(item.description).to eq(item_params[:description])
+    expect(item.unit_price).to eq(item_params[:unit_price])
+    expect(item.merchant_id).to eq(item_params[:merchant_id])
   end
 end
