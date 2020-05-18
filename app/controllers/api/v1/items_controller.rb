ac00e1495da0726084345ec767b1bf50908b4045
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-05-18 15:11:47 -0600

    Add create action and create strong params for new item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,9 +1,22 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    render json: Item.all
+    render json: ItemSerializer.new(Item.all)
   end
 
   def show
-    render json: Item.find(params[:id])
+    render json: ItemSerializer.new(Item.find(params[:id]))
+  end
+
+  def create
+    render json: ItemSerializer.new(Item.create(item_params))
+  end
+
+  private
+
+  def item_params
+    params.require('item').permit('name',
+                                  'description',
+                                  'unit_price',
+                                  'merchant_id')
   end
 end
