0f7f7c7bbcf5abcea68ffa886b2b56506c54fdd8
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-05-18 23:29:55 -0600

    Test search class method

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe Item do
   describe 'relationships' do
     it { should belong_to :merchant}
   end
+
+  describe 'class methods' do
+    it 'will return items based off of query param' do
+      merchant = create(:merchant)
+      create_list(:item, 3, merchant_id: merchant.id)
+      params = { "name" => Item.last.name[0..-4]}
+
+      expect(Item.search(params).first).to eq(Item.last)
+    end
+  end
 end
