c7db12823d4028c79ed41780b039533a0498e8e6
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-05-18 20:04:46 -0600

    Test it can return all items associated with the merchant

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -56,4 +56,18 @@ describe "Items API" do
     expect(response).to be_successful
     expect{Merchant.find(merchant.id)}.to raise_error(ActiveRecord::RecordNotFound)
   end
+
+  it "can return all items associated with the merchant" do
+    merchant1 = create(:merchant)
+    merchant2 = create(:merchant)
+    create_list(:item, 3, merchant_id: merchant1.id)
+    create_list(:item, 4, merchant_id: merchant2.id)
+
+    get "/api/v1/merchants/#{merchant1.id}/items"
+
+    items = JSON.parse(response.body)['data']
+
+    expect(response).to be_successful
+    expect(items.count).to eq(3)
+  end
 end
