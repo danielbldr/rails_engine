fbb2e407c9d8fc74132e3e142f54490f7b0b5287
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-05-19 15:03:41 -0600

    Update relationship in models

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,6 +1,9 @@
 class Invoice < ApplicationRecord
-  validates :status, presence: true
-
   belongs_to :customer
   belongs_to :merchant
+  has_many :items
+  has_many :invoice_items, through: :items
+  has_many :transactions
+
+  validates :status, presence: true
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,11 @@
 class Item < ApplicationRecord
+  belongs_to :merchant
+  has_many :invoice_items
+  has_many :invoices, through: :invoice_items
+
   validates :name, presence: true
   validates :description, presence: true
   validates :unit_price, presence: true
-
-  belongs_to :merchant
 
   def self.search(params)
     attribute = params.keys.first
