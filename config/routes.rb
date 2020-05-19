375e3f751df8812676f9edcc38ed89e690a2625b
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-05-18 20:05:32 -0600

    Add merchant and items scope to return the relationships

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :items, except: %I[new, edit]
-      resources :merchants, except: %I[new, edit]
+      resources :items, except: %I[new, edit] do
+        scope module: :items do
+          resources :merchant, only: [:index]
+        end
+      end
+      resources :merchants, except: %I[new, edit] do
+        scope module: :merchants do
+          resources :items, only: [:index]
+        end
+      end
     end
   end
 end
