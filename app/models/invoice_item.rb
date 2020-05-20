73e23cb2336965698122d109259d57fd25080d36
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-05-20 16:18:42 -0600

    Move total revenue method form Merchant to InvoiceItem

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,7 +1,16 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
+  has_many :transactions, through: :invoices
 
   validates :quantity, presence: true
   validates :unit_price, presence: true
+
+  def self.total_revenue(start_date, end_date)
+    InvoiceItem.joins(invoice: :transactions)
+               .where('date(invoices.created_at) BETWEEN ? AND ?',
+                      start_date, end_date)
+               .where("transactions.result = 'success'")
+               .sum('unit_price * quantity')
+  end
 end
