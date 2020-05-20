239943d7a79273d275c1cdce59a1cdac0a4e8a63
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-05-19 22:11:21 -0600

    Test it can return all merchants revenue across a period of time

--- spec/requests/api/v1/business_intelligence_endpoints_spec.rb
+++ spec/requests/api/v1/business_intelligence_endpoints_spec.rb
@@ -11,10 +11,10 @@ describe "Business Intelligence Endpoints" do
     @item2 = create(:item, merchant_id: @merchant2.id, unit_price: 5.00)
     @item3 = create(:item, merchant_id: @merchant3.id, unit_price: 1.00)
     @item4 = create(:item, merchant_id: @merchant4.id, unit_price: 10.00)
-    @invoice1 = Invoice.create!(customer_id: @customer.id, merchant_id: @merchant1.id, status: 'shipped')
-    @invoice2 = Invoice.create!(customer_id: @customer.id, merchant_id: @merchant2.id, status: 'shipped')
-    @invoice3 = Invoice.create!(customer_id: @customer.id, merchant_id: @merchant3.id, status: 'shipped')
-    @invoice4 = Invoice.create!(customer_id: @customer.id, merchant_id: @merchant4.id, status: 'shipped')
+    @invoice1 = Invoice.create!(customer_id: @customer.id, merchant_id: @merchant1.id, status: 'shipped', created_at: '2012-03-09')
+    @invoice2 = Invoice.create!(customer_id: @customer.id, merchant_id: @merchant2.id, status: 'shipped', created_at: '2012-03-14')
+    @invoice3 = Invoice.create!(customer_id: @customer.id, merchant_id: @merchant3.id, status: 'shipped', created_at: '2012-03-31')
+    @invoice4 = Invoice.create!(customer_id: @customer.id, merchant_id: @merchant4.id, status: 'shipped', created_at: '2012-03-14')
     InvoiceItem.create(item_id: @item1.id, invoice_id: @invoice1.id, quantity: 5, unit_price: @item1.unit_price)
     InvoiceItem.create(item_id: @item2.id, invoice_id: @invoice2.id, quantity: 2, unit_price: @item2.unit_price)
     InvoiceItem.create(item_id: @item3.id, invoice_id: @invoice3.id, quantity: 5, unit_price: @item3.unit_price)
@@ -49,5 +49,14 @@ describe "Business Intelligence Endpoints" do
     expect(merchant_names).to include(@merchant3.name)
     expect(merchant_names).to_not include(@merchant2.name)
     expect(merchant_names).to_not include(@merchant4.name)
+  end
+
+  it 'can return total revenue across all merchants for given date range' do
+    get '/api/v1/revenue?start=2012-03-09&end=2012-03-24'
+
+    revenue = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(revenue['data']['attributes']['revenue']).to eq(60)
   end
 end
